bdeefb24c5b50ef52ababc55b1b2b98cecc5a666
olo <snash@tutanota.com>
2021-05-20 16:47:07 -0300

    CRUD in progress | CRU completed

--- src/resolvers/Post.ts
+++ src/resolvers/Post.ts
@@ -1,21 +1,51 @@
 import { Post } from '../entities/Post'
-import {Arg, Ctx, Int, Query, Resolver} from 'type-graphql'
+import {Arg, Ctx, Int, Query, Mutation, Resolver} from 'type-graphql'
 import {MyContext} from '../types'
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(
-    @Ctx() {em}: MyContext): Promise<Post[]>
+    @Ctx() {em}: MyContext
+    ): Promise<Post[]> 
     {
-    return em.find(Post, {}) 
+      return em.find(Post, {}) 
   }
 
   @Query(() => Post, { nullable: true })
   post(
     @Arg('id', () => Int) id: number, 
-    @Ctx() {em}: MyContext): Promise<Post | null>
+    @Ctx() {em}: MyContext
+    ): Promise<Post | null> 
     {
-    return em.findOne(Post, {id}) 
+      return em.findOne(Post, {id}) 
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() {em}: MyContext
+    ): Promise<Post> 
+    {
+      const post = em.create(Post, {title})
+      await em.persistAndFlush(post)
+      return post
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title', () => String, {nullable: true}) title: string, 
+    @Ctx() {em}: MyContext
+    ): Promise<Post | null>
+    {
+      const updt = await em.findOne(Post, {id})
+      if(!updt){
+        return null
+      }
+      if( typeof title !== 'undefined'){
+        updt.title = title
+      }
+      return updt; 
   }
 }
