0074cdb32fd629444953cf8e11a81206898b7cf5
olo <snash@tutanota.com>
2021-06-02 10:21:42 -0300

    front & back set in Register_pg

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -4,17 +4,33 @@ import Container from '../components/Container';
 import InputField from '../components/InputField';
 import { Box } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
+import { useMutation } from 'urql';
 
 interface registerProps {
 
 }
+const REGISTER_MUT = `
+  mutation Register($username:String!, $password:String!){
+    register(options: {username: $username, password: $password}){
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+`
 
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT)
   return (
     <Container responsv='small'>
       <Formik
         initialValues={{username: '', password:''}}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => register(values)}
       >
         {({isSubmitting}) => (
           <Form>
