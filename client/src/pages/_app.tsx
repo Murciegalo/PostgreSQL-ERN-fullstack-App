fa96ebec03802b9b3e22fe65f8739c3ed9310717
olo <snash@tutanota.com>
2021-06-02 10:13:00 -0300

    urql graphql_Client set

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,34 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-
+import {createClient, Provider} from 'urql';
 import theme from '../theme'
 
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+  // fetchOptions: () => {
+  //   // const token = getToken();
+  //   return {
+  //     headers: { authorization: token ? `Bearer ${token}` : '' },
+  //   };
+  // },
+});
+
 function MyApp({ Component, pageProps }) {
+  
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   )
 }
 
 export default MyApp
+
+
